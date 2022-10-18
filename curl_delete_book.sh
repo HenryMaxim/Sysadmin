60aa15126ec018fbb09ba070bbb4e5945722c651
MaximHenry <maxim.henry@student.odisee.be>
2022-10-18 08:59:22 +0000

    deleting files

--- curl_delete_book.sh
+++
@@ -1,5 +0,0 @@
-APIKEY="cisco|r7zP2UOen9jQAPMCy-kSj5vzl0HJotU_ruqp0T5KTmo"
-BoOOK=999
-DELETE_URL="http://library.demo.local/api/v1/books/$BOOK"
-echo $DELETE_URL
-curl -X DELETE $DELETE_URL  -H "accept: application/json" -H "X-API-KEY: $APIKEY" -H "Content-Type: application/json"
